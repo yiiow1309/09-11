c61669274c4b0ea2884e7ec269d428a9e95df209
yiiow1309 <55523982+yiiow1309@users.noreply.github.com>
2020-09-11 10:17:34 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LJS_Report1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학번: 201987014");
+            Console.WriteLine("이름: 이재성");
+        }
+    }
+}
